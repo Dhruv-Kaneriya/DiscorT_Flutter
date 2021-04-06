55c4cbf64277b554056f379cceae34a42719fa51
SavanRajyaguru <65443618+SavanRajyaguru@users.noreply.github.com>
2021-04-06 09:25:21 +0530

    Update Video

---
+++ lib/Modal/Videos.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoScreen extends StatefulWidget {
+  @override
+  _VideoScreenState createState() => _VideoScreenState();
+}
+
+class _VideoScreenState extends State<VideoScreen> {
+
+  VideoPlayerController _controller;
+  Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+        'https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4');
+    _initializeVideoPlayerFuture = _controller.initialize();
+    _controller.setVolume(0.0);
+    _controller.setLooping(true);
+    _controller.play();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initializeVideoPlayerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: VideoPlayer(_controller),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'Modal/Videos.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
@@ -18,13 +19,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'DiscorT'),
+      home: MyHomePage(title: 'DiscorT',),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key key , this.title}) : super(key : key);
   final String title;
 
   @override
@@ -32,128 +33,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final myController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
-  DateTime date2;
-  @override
-  void dispose() {
-    myController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        elevation: 5,
-        backgroundColor: Colors.red,
-        title: Text(widget.title),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            DateField(),
-            BasicDateTimeField(),
-            Form(
-              key: _formKey,
-              child: TextFormField(
-                decoration: InputDecoration(hintText: "Enter Number"),
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                controller: myController,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some Number';
-                  }
-                  return null;
-                },
-              ),
+      backgroundColor: Color(0xFF1f1f27),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      height: size.height * 0.1,
+                      child: Placeholder()
+                    ),
+                    Container(
+                      height: size.height * 0.8,
+                      width: double.infinity,
+                      child: Expanded(child: VideoScreen()),
+                    ),
+                    Container(
+                      color: Colors.transparent,
+                      height: size.height * 0.1,
+                      child: Placeholder(),
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 200,
+                  color: Colors.red,
+                ),
+              ],
             ),
-          ],
+          ),
         ),
-        // child: TextField(
-        //   controller: myController,
-        //   decoration: InputDecoration(
-        //     hintText: "Write any thing"
-        //   ),
-        // ),
       ),
-      floatingActionButton: FloatingActionButton(
-        // When the user presses the button, show an alert dialog containing
-        // the text that the user has entered into the text field.
-        onPressed: () {
-          if(_formKey.currentState.validate()){
-            return ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                    duration: Duration(milliseconds: 600),
-                    elevation: 10,
-                    backgroundColor: Colors.deepOrange,
-                    content: Text(myController.text,style: TextStyle(fontSize: 20.0),)
-                )
-            );
-          }
-          // return showDialog(
-          //   context: context,
-          //   builder: (context) {
-          //     return AlertDialog(
-          //       // Retrieve the text the that user has entered by using the
-          //       // TextEditingController.
-          //       content: Text(myController.text),
-          //     );
-          //   },
-          // );
-        },
-        tooltip: 'Show me the value!',
-        child: Icon(Icons.text_fields),
-      ),
-    );
-  }
-}
-
-class DateField extends StatelessWidget {
-  final format = DateFormat("dd-MM-yyyy");
-  @override
-  Widget build(BuildContext context) {
-    return DateTimeField(
-      format: format,
-      decoration: InputDecoration(hintText: "Enter Date"),
-      onShowPicker: (context, currentValue) {
-        return showDatePicker(
-            context: context,
-            firstDate: DateTime.now(),
-            initialDate: currentValue ?? DateTime.now(),
-            lastDate: DateTime(2100));
-      },
-    );
-  }
-}
-
-class BasicDateTimeField extends StatelessWidget {
-  final format = DateFormat("yyyy-MM-dd HH:mm");
-  @override
-  Widget build(BuildContext context) {
-    return DateTimeField(
-      format: format,
-      decoration: InputDecoration(hintText: "Enter Date & Time"),
-      onShowPicker: (context, currentValue) async {
-        final date = await showDatePicker(
-            context: context,
-            firstDate: DateTime.now(),
-            initialDate: currentValue ?? DateTime.now(),
-            lastDate: DateTime(2100));
-        if (date != null) {
-          final time = await showTimePicker(
-            context: context,
-            initialTime:
-            TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
-          );
-          return DateTimeField.combine(date, time);
-        } else {
-          return currentValue;
-        }
-      },
     );
   }
 }
